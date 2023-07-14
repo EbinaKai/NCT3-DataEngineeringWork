f8dd12de7f9d95b4909e09fc57e730e9073b73aa
Ebina Kai <85666313+EbinaKai@users.noreply.github.com>
2023-07-14 13:41:32 +0900

    [add] index.php

---
+++ htdocs/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="EbinaKai">
+    <title>ポータルページ</title>
+    <link rel="icon" href="favicon.ico">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+
+    <link rel="stylesheet" href="CSSsample.css">
+</head>
+<body>
+  <div id="wrapper">
+    <h1>課題一覧</h1>
+    <ul>
+      <li><a href="php01/php_sample.php">課題1</a></li>
+      <li><a href="php02/index.html">課題2</a></li>
+      <li><a href="php03/index.php">課題3</a></li>
+      <li><a href="php03/index.php">課題4（課題3と同じファイル）</a></li>
+    </ul>
+    <footer>
+		  © Ebina Kai
+    </footer>
+    </div>
+</body>
+</html>
--- htdocs/php04/innerjoin.sql
+++
@@ -1,3 +0,0 @@
-SELECT s.gakusei_id, s.kamoku_id, s.seiseki, k.kamoku_id, k.kamoku_mei
-FROM seiseki_t AS s
-INNER JOIN kamoku_t AS k ON k.kamoku_id = s.kamoku_id;
